d681cf742b256d1c28104ae156c574ac7f023fc5
Abideen <misterjrshittu@gmail.com>
2023-07-22 16:26:34 +0100

    updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,12 @@ const Navbar = () => {
   return (
     <Stack>
       <Link>
-        <img src={logo} alt='Hey' style={{width: '100px'}}/>
+        <img src={logo} alt='Hey' style={{width: '48px', height: '48px', margin: '0 20px'}}/>
       </Link>
+      <stack>
+        <Link to='/' style={{textDecoration: 'none', color:'#3A1212', borderBottom:'3px solid #ff2625', display:'block', width:'40px'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration: 'none', color: '#ce5dfe'}}>Exercises</a>
+      </stack>
     </Stack>
   )
 }
