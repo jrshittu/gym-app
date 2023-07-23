da9c9824c8d2b923057d0e3eeea6321162b5bb6e
Abideen <misterjrshittu@gmail.com>
2023-07-24 00:09:21 +0100

    All through with banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -20,7 +20,7 @@ function HeroBanner() {
       </Typography>
       <Button variant='contained' color='error' href='#exercises'>Find a suitable Exercise</Button>
       <Typography
-         fontWeight={600} color='#ff2625' fontSize='200px'
+         fontWeight={600} color='#ff2625' fontSize='200px' mt='12px' mb='40px'
          sx={{opacity: 0.1, display: { lg: "block", xs:"none"}
          
         }}
