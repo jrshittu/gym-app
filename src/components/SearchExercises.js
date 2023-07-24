b2b89435b15bb7b207d4d6c0246b5c886a6816b4
Abideen <misterjrshittu@gmail.com>
2023-07-24 17:39:16 +0100

    Update SearchExercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -7,21 +7,16 @@ function SearchExercises() {
       <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '30px'}}} textAlign='center' mb='50px'>
         Essential Exercise You <br />
         Can do
-      </Typography>
-      <Box position="relative" mb="72px">
-        <TextField
-          height="76px"
-          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-          value={search}
-          onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          placeholder="Search Exercises"
-          type="text"
-        />
-        <Button className="search-btn" sx: {{ backgroundColor: '#ff2625', color:'#fff', textTransform:'none', width:{lg:'173px', xs:'70px'}, positon:'absolute', height: '56px', right: '0px', fontSize: {lg: '20px', xs: '14px'}}} onClick={handleSearch}>
-            Search
+      </Typography>    
+      <Box position='relative' mb='72px'>
+        <TextField height='46px' sx={{ fontWeight: '700', border:'none', borderRadius:'4px', width:{lg:'1125px', xs:'350px'}, backgroundColor: '#fff', borderRadius: '40px'}} type="text"
+        placeholder='Search Exercises' />
+        <Button className='search-btn' sx={{backgroundColor: '#ff2625', position: 'absolute', right: '0px', color:'#fff', textTransform:'none', width:{lg: '170px', xs:'80px'}, height:'56px', fontSize: {lg: '20px', xs:'14px'}, border: '1px solid #fff'}}>
+          Search
         </Button>
       </Box>
-    </Stack>
+      
+      </Stack>
   )
 }
 
