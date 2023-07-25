b5514c3b80aa9e45abce8096818fdd0c75412466
Abideen <misterjrshittu@gmail.com>
2023-07-25 23:04:39 +0100

    Updated HorizontalScrollbar.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,24 @@ import { useState } from 'react';
 import { Box } from '@mui/material';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
+  const [ bodyPart, setBodyPart ] = useState('all');
+  const [ exercises, setExercises ] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
+      <SearchExercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}
+      />
+      <Exercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}
+      />
       
     </Box>
   )
