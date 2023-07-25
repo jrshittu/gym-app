d3d74bc20771f36433631a17dbbff3cf277e5c15
Abideen <misterjrshittu@gmail.com>
2023-07-25 23:23:13 +0100

    Started Horizontal Scroll bar

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,9 +3,21 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = (item, setBodyPart, bodyPart) => {
   return (
-    <Stack>
+    <Stack type='button' alignItems= 'center' justifyContent='center' className='bodyPart-card'
+        sx={bodyPart === item ? {
+            borderTop: "4px solid #ff2625", backgroundColor: "#fff",
+            width: "270px", borderBottomLeftRadius: "20px",
+            height: "280px",
+            cursor: "pointer",
+            gap: "47px" 
+        
+        } : {
+            background: '#fff',    
+        } 
+    }
+    >
       <img src={Icon} />
     </Stack>
   )
