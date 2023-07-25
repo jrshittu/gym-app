5338b19131867c684f0057e0c83ca01f4146e521
Abideen <misterjrshittu@gmail.com>
2023-07-25 17:59:35 +0100

    Updated components

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Stack, TextField, Button, Box, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Box, Stack, Typography, TextField, Button} from '@mui/material';
+
 import { exerciseOptions, fetchData } from '../utils/fetchData';
-import HorizontalScrollbar from '../components/HorizontalScrollbar';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-function SearchExercises() {
-  const [ search, setSearch] = useState('');
-  const [ exercises, setExercises ] = useState([]);
+const SearchExercises = ( { bodyPart, setBodyPart, setExercises })  => {
+  const [search, setSearch] = useState('');
+
+  // fetching exercise categories
   const [bodyParts, setBodyParts] = useState([]);
-
   useEffect(() => {
-    const fetchExercisesData = async () => {
-      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
-
+    const fetchExercisesData =  async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',exerciseOptions);
       setBodyParts(['all', ...bodyPartsData]);
-    };
-
+    }
     fetchExercisesData();
   }, []);
+  
 
-  const handleSearch = async () => {
-    if(search) {
-      const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-      
-      const searchedExercises = exerciseData.filter(
+
+  const handleSearch = async () =>{
+    if (search) {
+      const exercisesData =  await fetchData('https://exercisedb.p.rapidapi.com/exercises',exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
         (exercise) => exercise.name.toLowerCase().includes(search)
         || exercise.target.toLowerCase().includes(search)
         || exercise.equipment.toLowerCase().includes(search)
         || exercise.bodyPart.toLowerCase().includes(search)
-      );
-      setExercises('');
+      )
+      setSearch('');
       setExercises(searchedExercises);
+      window.scrollTo({ top: 1800, behavior: 'smooth' });
     }
-  };
+  }
 
   return (
-    <Stack alignItems='center' justifyContent='center' mt='17px' p='10px'>
+    <Stack alignItems='center' justifyContent='center' mt='2px' p='10px'>
       <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '30px'}}} textAlign='center' mb='50px'>
         Essential Exercise You <br />
         Can do
@@ -57,29 +59,26 @@ function SearchExercises() {
         placeholder='Search Exercises' 
         onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
-        <Button 
-        className='search-btn' 
+        
+        <Button className="search-btn" 
         sx={{
-          backgroundColor: '#ff2625', 
-          position: 'absolute', 
-          right: '0px', color:'#fff', 
-          textTransform:'none', 
-          width:{lg: '170px', xs:'80px'}, 
-          height:'56px', 
-          fontSize: {lg: '20px', xs:'14px'}, 
-          border: '1px solid #fff'
-          }}
-          onClick={handleSearch}
-          >
-          Search
-        </Button>
+          backgroundColor: '#ff2625',
+          color: '#fff',
+          textTransform: 'none',
+          // borderRadius: '40px',
+          width: {lg: '175px', sm:'80px'},
+          fontSize: {lg: '20px',  sm:'14px'},
+          height: '56px',
+          position: 'absolute',
+          right: '0',
+        }} onClick={handleSearch}>
+          Search</Button>
       </Box>
-      <Box sx={{position:'relative', width: '100%', p:'20px'}}>
-          <HorizontalScrollbar data={bodyParts} />
-      </Box>
-      
-      </Stack>
+        <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
+          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyParts />
+        </Box>
+    </Stack>
   )
 }
 
-export default SearchExercises;
+export default SearchExercises
