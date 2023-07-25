c8ffab6a729bcc8b96d974b7d38bb4eac3cd4457
Abideen <misterjrshittu@gmail.com>
2023-07-25 18:00:47 +0100

    Updated components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import { Box } from '@mui/material';
-import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
