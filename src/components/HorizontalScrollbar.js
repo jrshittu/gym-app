79f85fb7f0e4057b8318a7c770dff2517283664a
Abideen <misterjrshittu@gmail.com>
2023-07-26 14:37:08 +0100

    Started enabling horizontal scroll

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+
+
 import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
