e1152e3c091dc53db770b619cd933625c6c9757e
Abideen <misterjrshittu@gmail.com>
2023-07-26 15:25:48 +0100

    fix BodyPart.js convert the parameters to objects

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,7 +3,7 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/assets/icons/gym.png';
 
-const BodyPart = (item, setBodyPart, bodyPart) => {
+const BodyPart = ( {item, setBodyPart, bodyPart }) => {
   return (
     <Stack type='button' alignItems= 'center' justifyContent='center' className='bodyPart-card'
         sx={{
