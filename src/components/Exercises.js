c05ad427efcc54d876d4c89d7da69656c6029311
Abideen <misterjrshittu@gmail.com>
2023-07-27 17:52:46 +0100

    Added Title and other Items to exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -9,9 +9,15 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
     <Box id="exercises"
         sx={{mt: {lg:'110px'}}} mt='50px' p='20px'
     >
-      <Typography variant='h3' mb='46px'>
+      <Typography variant='h2' mb='46px'>
         Showing Results
       </Typography>
+      <Stack direction='row' sx={{gap: { lg: '110px', xs: '50px'}}}
+        flexWrap='wrap' justifyContent='center'
+      >
+
+
+      </Stack>
     </Box>
   )
 }
